3bb3a36e2f84c32ec3722cb1cda629378416ae08
Bas Boot <github@basboot.nl>
2022-05-30 20:30:05 +0200

    Add helper methods (untested!)

--- puzzle.py
+++ puzzle.py
@@ -29,6 +29,73 @@ class Puzzle:
     def is_occupied(self, point):
         return point in self.occupied
 
+    def next_block(self, position):
+        x = position[0]
+        y = position[1]
+
+        x += 1
+        if x == self.m:
+            y = y + 1
+        if y == self.n:
+            return None
+
+        return (x, y)
+
+    def layout_fits(self, layout, position):
+        for block in layout.blocks:
+            x = layout[0] + block[0]
+            y = layout[1] + block[1]
+
+            # check puzzle boundaries
+            if x < 0 or x >= self.m:
+                return False
+            if y < 0 or y >= self.n:
+                return False
+            if self.is_occupied((x, y)):
+                return False
+
+        return True
+
+    def add_piece(self, piece_id, layout_id, position):
+
+        for block in pieces[piece_id].layouts[layout_id].blocks:
+            x = position[0] + block[0]
+            y = position[1] + block[1]
+
+            # TODO: add asserts
+            # add to occupied
+            self.occupied.add((x, y))
+
+        # pieces on the board
+        # piece id => (layout id, position)
+        self.solution[piece_id] = (layout_id, position)
+
+    def remove_piece(self, piece_id, layout_id, position):
+
+        for block in pieces[piece_id].layouts[layout_id].blocks:
+            x = position[0] + block[0]
+            y = position[1] + block[1]
+
+            # TODO: add asserts
+            # add to occupied
+            self.occupied.remove((x, y))
+
+        # pieces on the board
+        # piece id => (layout id, position)
+        del self.solution[piece_id]
+
+# TODO: create copy, or cleanup intermediate solutions?
+def solve(current_position, current_puzzle):
+    if current_puzzle.is_occupied(current_position):
+        # next pos
+        pass
+    else:
+        # current pos
+        # loop pieces * layouts
+        # fit?
+        # last piece => ready, else next position
+        # all done? backtrack
+        pass
 
 if __name__ == '__main__':
     pieces = [Piece([Layout([(0, 1)]), Layout([(0, 1), (1, 1)])])]
